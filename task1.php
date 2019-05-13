272f47b55cdf6750590e543e25d725b10d8653af
MKgeist <49156049+MKgeist@users.noreply.github.com>
2019-05-13 17:10:17 +0900

    課題です。

--- task1.php
+++ task1.php
@@ -14,6 +14,82 @@
   くだもの名をキー、個数を値にした連想配列を定義してください。キーは日本語ではなく英数字で指定してください。例 リンゴ→appleなど
 
 */
+<?php
+function test(){
+echo "Hello World";
+}
+
+echo test();
+ ?>
+
+ <br>
+
+ <?php
+function test1($i){
+if($i >= 0)
+echo "Hello World";
+}
+echo test1(1);
+  ?>
+<br>
+
+<?php
+$a= "Hello World";
+
+function test2($a){
+echo $a;
+}
+echo test2($a);
+ ?>
+ <br>
+ <?php
+
+function test3($a){
+  echo $a . "!!";
+}
+$a= "Hello World";
+echo test3($a);
+  ?>
+<br>
+<?php
+$a = "Hello";
+$b = " World";
+
+function test4($a,$b){
+echo $a . $b ."!!";
+}
+echo test4($a,$b);
+ ?>
+ <br>
+
+ <?php
+function test5($a){
+if ($a = 1 or 3){
+  echo "奇数";
+}
+elseif ($a = 2 or 4){
+  echo "偶数";
+}
+}
+echo test5(4);
+  ?>
+  <br>
+  <?php
+$arr = [1,2,3,4,5,6];
+// ７が分かりませんでした。
+function test6($arr){
+ for ($a = 0; $a <= 6; $a++){
+echo $arr[$a];
+}}
+echo test6(0);
+?>
+
+<br>
+
+<?php
+$a=array( "apple"=>6, "strawberry"=> 3,"lemon"=> 8,"watermelon"=> 5, "pineapple"=>4,"Kiwi"=>2);
+print_r($a);
+ ?>
 
 
 
